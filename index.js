5ef46e9dac721050d6908ffffc2664a4bcb50c18
xmp-er <bankofhawkgatsu@gmail.com>
2023-03-12 08:44:26 +0530

    Full project built

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ curr_player=document.querySelector("[curr_player]");
 
 game_btn=document.querySelector("[game_btn]");
 
+
 let check_pos=[
     [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
 ]
